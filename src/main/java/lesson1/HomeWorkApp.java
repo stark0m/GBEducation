a5868b3c62c243b28f43fcd01b217bbadca95260
Stark0m <011283im@gmail.com>
2022-02-04 12:38:19 +0300

    Lesson 1 Created

---
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,49 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        System.out.println();
+    }
+
+    private static void compareNumbers() {
+        int a = 21;
+        int b = 14;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    private static void printColor() {
+        int value = 1001;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void checkSumSign() {
+        int a = 7;
+        int b = 5;
+        System.out.println("Выбраны числа а=" + a + ",b=" + b);
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    private static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
