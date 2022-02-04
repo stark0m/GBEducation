1c6aea4804a2e7b6de02a941580d04e0834437e5
Stark0m <011283im@gmail.com>
2022-02-04 12:39:46 +0300

    тестовые доработки

--- src/main/java/lesson1/HomeWorkApp.java
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -7,6 +7,7 @@ public class HomeWorkApp {
         printColor();
         compareNumbers();
         System.out.println();
+        System.out.println();
     }
 
     private static void compareNumbers() {
