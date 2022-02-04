80ee7882d8c479746c5368d8c4e4faf9f1eea30e
Stark0m <011283im@gmail.com>
2022-02-04 13:22:06 +0300

    Уроки 2 и 3

---
+++ src/main/java/lesson2/Temp.java
@@ -0,0 +1,41 @@
+package lesson2;
+
+//public class less2 {
+//    public static void main(String[] args) {
+//        final int CONST = 3;
+//
+//        for (int k = 0; k < ; k++) {
+//
+//        }
+//    }
+//
+//    static void printNumsAndResult(int a, int b) {
+//        String season = "";
+//        String season2 = switch (a1) {
+//            case 1 -> "wint1";
+//            case 2 -> "wint2";
+//            case 3 -> "wint3";
+//            case 4 -> "wint4";
+//            default -> "Uncnow";
+//        };
+//        System.out.printf("%d + %d = %d%n", a, b, result);
+//        switch (a1) {
+//            case 1:
+//                season = "winter";
+//                break;
+//            case 2:
+//                season = "winterw";
+//                break;
+//            case 3:
+//                season = "winter3";
+//                break;
+//            case 4:
+//                season = "winter4";
+//                break;
+//            default:
+//                season = "Uncnow";
+//        }
+//        return;
+//        season;
+//    }
+//}
