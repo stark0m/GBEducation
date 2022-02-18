62c49bca0471217993686a912c39297f5e2e6c97
ilya <011283im@gmail.com>
2022-02-18 12:26:17 +0300

    Доработка сортировки выбором с заменой максимального и минимального значений за один проход

--- src/main/java/Algorithms/Lesson2/Main.java
+++ src/main/java/Algorithms/Lesson2/Main.java
@@ -1,5 +1,16 @@
 package Algorithms.Lesson2;
 
-public class Main {
+import Algorithms.Lesson2.Sorting.MyArray;
+import Algorithms.Lesson2.Sorting.SelectionSort;
 
+public class Main {
+    public static void main(String[] args) {
+        MyArray.isRepeat = false;
+        MyArray.isShuffle = true;
+        final int SIZE = 10;
+        Integer[] arr = MyArray.getArray(SIZE, 3);
+//        arr = new Integer[]{6, 17, 4, 0, 11, 2, 9, 12, 21, 13};
+        SelectionSort.sort(arr);
+
+    }
 }
