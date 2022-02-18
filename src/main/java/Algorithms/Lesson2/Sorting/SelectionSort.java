5ac4c2b47e4e75eee1fe499600bc7a04939babef
ilya <011283im@gmail.com>
2022-02-18 12:02:43 +0300

    Курс Алгоритмы. ДЗ 2. Создание прокта

---
+++ src/main/java/Algorithms/Lesson2/Sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package lesson2.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+//            System.out.println("i: " + i + Arrays.toString(arr));
+        }
+    }
+}
