5984aec7ccdebe30e3f885620b2f3f73f2a3125a
ilya <011283im@gmail.com>
2022-02-18 13:10:58 +0300

    генерация массива ноутбуков, не рабочая версия

--- src/main/java/Algorithms/Lesson2/Main.java
+++ src/main/java/Algorithms/Lesson2/Main.java
@@ -1,16 +1,35 @@
 package Algorithms.Lesson2;
 
+import Algorithms.Lesson2.Laptop.Laptop;
 import Algorithms.Lesson2.Sorting.MyArray;
 import Algorithms.Lesson2.Sorting.SelectionSort;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        MyArray.isRepeat = false;
+/*        MyArray.isRepeat = false;
         MyArray.isShuffle = true;
         final int SIZE = 10;
         Integer[] arr = MyArray.getArray(SIZE, 3);
 //        arr = new Integer[]{6, 17, 4, 0, 11, 2, 9, 12, 21, 13};
-        SelectionSort.sort(arr);
+        SelectionSort.sort(arr);*/
+
+        final  int LAPTOP_COUNT = 10;
+        Laptop laptop1 = new Laptop();
+        Laptop[] laptops= new Laptop[LAPTOP_COUNT];
+        for (Laptop laptop : laptops) {
+            laptop = new Laptop();
+            laptop.genLaptop();
+            System.out.println(laptop);
+        }
+
+
+        for (Laptop laptop : laptops) {
+            System.out.println(laptop);
+
+        }
+        System.out.println(Arrays.toString(laptops));
 
     }
 }
