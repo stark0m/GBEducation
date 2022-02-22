c14ce48dbc5f97678ef63ae7f0760fc34bbe2996
ilya <011283im@gmail.com>
2022-02-22 12:04:46 +0300

    Реализован класс декью.

--- src/main/java/Algorithms/Lesson3/Deque/Deque.java
+++ src/main/java/Algorithms/Lesson3/Deque/Deque.java
@@ -1,11 +1,19 @@
 package Algorithms.Lesson3.Deque;
 
 public interface Deque<E> {
-    boolean insert(E value);
 
-    E remove();
 
-    E peekFront();
+    boolean insertLeft(E value);
+
+    boolean insertRight(E value);
+
+    E removeLeft();
+
+    E removeRight();
+
+    E peekLeft();
+
+    E peekRight();
 
     int size();
 
---
+++ src/main/java/Algorithms/Lesson3/DequeTest.java
@@ -0,0 +1,73 @@
+package Algorithms.Lesson3;
+
+import Algorithms.Lesson3.Deque.DequeImpl;
+import Algorithms.Lesson3.Deque.Deque;
+
+
+public class DequeTest {
+    public static void main(String[] args) {
+        testDeue();
+    }
+
+    private static void testDeue() {
+
+        Deque<Integer> deque = new DequeImpl<>(12);
+        System.out.println("add Left: " + deque.insertLeft(1));
+        deque.display();
+        System.out.println("add Right: " + deque.insertRight(2));
+        deque.display();
+        System.out.println("add Right: " + deque.insertRight(3));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(4));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(5));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(6));
+        deque.display();
+
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+        System.out.println("remove left: " + deque.removeLeft());
+        deque.display();
+
+        System.out.println("-----------------");
+        System.out.println("add Right: " + deque.insertRight(7));
+        deque.display();
+        System.out.println("add Right: " + deque.insertRight(8));
+        deque.display();
+        System.out.println("add Right: " + deque.insertRight(9));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(10));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(11));
+        deque.display();
+        System.out.println("add Left: " + deque.insertLeft(12));
+        deque.display();
+
+        System.out.println("-----------------");
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+        System.out.println("remove right: " + deque.removeRight());
+        deque.display();
+    }
+}
