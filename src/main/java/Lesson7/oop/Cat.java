a6cbab4b0db35596a4a13aafb08d597d9ddc6b1a
ilya <011283im@gmail.com>
2022-02-23 11:49:45 +0300

    доработаны классы кот и тарелка в соответствии с поставленной задачей

--- src/main/java/Lesson7/oop/Cat.java
+++ src/main/java/Lesson7/oop/Cat.java
@@ -4,14 +4,46 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Cat {
     private String name;
+    private static final int IT_IS_FULL = 10;
+    private int satiety;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
 
     public Cat(String name) {
         this.name = name;
+        this.setSatiety(ThreadLocalRandom.current().nextInt(IT_IS_FULL));
     }
 
     public void eat(Plate plate) {
-        plate.decreaseFood(ThreadLocalRandom.current().nextInt(4) + 3);
+        plate.decreaseFood(this);
 
-        //toDo
+
+
+    }
+    public int needToBeFull (){
+        return IT_IS_FULL-satiety;
+    }
+
+    public void ate() {
+        setSatiety(IT_IS_FULL);
+    }
+
+    public boolean isFull() {
+        return IT_IS_FULL==getSatiety();
+    }
+
+    public void printInfo() {
+        System.out.printf("Кот %s сыт на %s из %s, он хочет сьесть %s еды%n",this.name,this.getSatiety(),IT_IS_FULL, needToBeFull());
     }
 }
--- src/main/java/Lesson7/oop/Test.java
+++ src/main/java/Lesson7/oop/Test.java
@@ -1,16 +1,38 @@
 package Lesson7.oop;
 
+import java.util.Random;
+
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        Cat cat = new Cat("Мартин Лютер Кот");
-        Plate plate = new Plate(100);
+        String[] catName = new String[]{"Барсик", "Васька","Кокос","Космос","Белка","Стрелка","Беляш"};
+        final int CAT_COUNT= 100;
+        final int EAT_COUNT= 100;
+        Cat[] catArray = new Cat[CAT_COUNT];
+
+
+        for (int i = 0; i < CAT_COUNT; i++) {
+            catArray[i] = new Cat(catName[new Random().nextInt(catName.length)]);
+
+        }
+
+
+        Plate plate = new Plate(EAT_COUNT);
 
         plate.printInfo();
 
-        while (plate.getFoodCount() > 0) {
+        for (Cat cat : catArray) {
+            if (cat.isFull()) {
+                continue;
+            }
+        cat.printInfo();
             cat.eat(plate);
+            while (!cat.isFull()){
+                System.out.println("Коты еще не наелись, а еда уже закончилась, срочно добавим еды в тарелку!");
+                plate.addFood(new Random().nextInt(50));
+                plate.printInfo();
+                cat.eat(plate);
+            }
             plate.printInfo();
-            Thread.sleep(3000);
         }
 
     }
